ba480547ab422e0b548d1cd6e57447eba399a16b
Lander Peeters <lander.peeters13@gmail.com>
2022-12-22 01:13:01 +0100

    basic udp network test code

---
+++ server.py
@@ -0,0 +1,28 @@
+import socket
+import sys
+
+class NetGame:
+	def __init__(self):
+		self.state
+
+class NetEntity:
+	def __init__(self):
+		self.x
+		self.y
+
+class NetPlayer(NetEntity):
+	def __init__(self):
+		self.id
+
+class NetProjectile(NetEntity):
+	def __init__(self):
+		self.owner_id
+
+def main():
+
+	...
+
+	return 0
+
+if __name__ == "__main__":
+	sys.exit(main())
